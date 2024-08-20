96f97a130946d17933524358f6d9bcc612e62460
shashyv <shashyv@gmail.com>
2024-08-20 17:29:43 +0530

    Updated links in News Card

--- src/components/News.js
+++ src/components/News.js
@@ -2,20 +2,65 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
+  articles= [
+    {
+        "source": {
+            "id": "bbc-sport",
+            "name": "BBC Sport"
+        },
+        "author": null,
+        "title": "The Hundred 2024 LIVE: Watch Oval Invincibles vs London Spirit eliminator on TV - cricket score, stream & commentary",
+        "description": "Oval Invincibles face London Spirit in the women’s Hundred Eliminator – watch live coverage and follow radio commentary & score updates.",
+        "url": "http://www.bbc.co.uk/sport/cricket/live/ckvg5jz9nvqt",
+        "urlToImage": "https://static.files.bbci.co.uk/ws/simorgh-assets/public/sport/images/metadata/poster-1024x576.png",
+        "publishedAt": "2024-08-17T13:37:15.7128005Z",
+        "content": "Oval Invincibles: Lauren Winfield-Hill (capt, wk), Paige Scholfield, Alice Capsey, Marizanne Kapp, Jo Gardner, Laura Harris, Mady Villiers, Amanda-Jade Wellington, Ryana Macdonald-Gay, Sophia Smale, … [+205 chars]"
+    },
+    {
+        "source": {
+            "id": "espn-cric-info",
+            "name": "ESPN Cric Info"
+        },
+        "author": null,
+        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+        "publishedAt": "2020-04-27T11:41:47Z",
+        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+    },
+    {
+        "source": {
+            "id": "espn-cric-info",
+            "name": "ESPN Cric Info"
+        },
+        "author": null,
+        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+        "publishedAt": "2020-03-30T15:26:05Z",
+        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+    }
+]
+constructor(){
+super();
+this.state={
+  articles:this.articles,
+  loading:false
+}
+}
+
   render() {
     return (
       <div>
         <h1>Top Headlines Today</h1>
-        <div className="row">
-          <div className="col-md-4">
-            <NewsItem title="Hello World" newsUrl="Todo" description="Ebola La La La La La" imageUrl="https://static.files.bbci.co.uk/ws/simorgh-assets/public/sport/images/metadata/poster-1024x576.png" />
+         <div className="row">
+        {this.state.articles.map((element)=>{
+          return <div className="col-md-4" key={element.url}>
+            <NewsItem title={element.title.slice(0,45)} newsUrl={element.url} description={element.description.slice(0,88)} imageUrl={element.urlToImage} />
           </div>
-          <div className="col-md-4">
-            <NewsItem title="Hello World" description="Ebola La La La La La" />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="Hello World" description="Ebola La La La La La" />
-          </div>
+        })}
         </div>
       </div>
     );
